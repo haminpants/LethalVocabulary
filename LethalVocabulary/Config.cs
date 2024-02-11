c265b57c3c3c3e154900b0d725d38e06ee7ed897
Sam <samtfong@hotmail.com>
2024-02-11 14:38:40 -0800

    Initial commit
    Contains first working version of the mod

---
+++ LethalVocabulary/Config.cs
@@ -0,0 +1,99 @@
+﻿using BepInEx.Configuration;
+
+namespace LethalVocabulary;
+
+public class Config {
+    public static ConfigEntry<bool> ExtendedWordsEnabled;
+    public static ConfigEntry<int> CategoriesPerMoon;
+    public static ConfigEntry<string> SnareFleaWords;
+    public static ConfigEntry<string> BunkerSpiderWords;
+    public static ConfigEntry<string> HoardingBugWords;
+    public static ConfigEntry<string> BrackenWords;
+    public static ConfigEntry<string> ThumperWords;
+    public static ConfigEntry<string> HygrodereWords;
+    public static ConfigEntry<string> GhostGirlWords;
+    public static ConfigEntry<string> SporeLizardWords;
+    public static ConfigEntry<string> NutCrackerWords;
+    public static ConfigEntry<string> JesterWords;
+    public static ConfigEntry<string> MaskedWords;
+    public static ConfigEntry<string> EyelessDogWords;
+    public static ConfigEntry<string> ForestKeeperWords;
+    public static ConfigEntry<string> EarthLeviathanWords;
+    public static ConfigEntry<string> BaboonHawkWords;
+    public static ConfigEntry<bool> DebugSTTWords;
+
+    public Config(ConfigFile cfg) {
+        ExtendedWordsEnabled = cfg.Bind("Gameplay",
+            "EnableExtendedWords",
+            false,
+            "Words in ALL CAPS are considered \"extended words\" and will not be included unless this setting is true.");
+        CategoriesPerMoon = cfg.Bind("Gameplay",
+            "CategoriesPerMoon",
+            1,
+            "The number of categories to choose per moon.");
+        SnareFleaWords = cfg.Bind("Dictionary.Entities",
+            "SnareFlea",
+            "snare,flea,fleas,centipede,centipedes,HEAD,CRAB,CRABS,FACE,HUGGER,HUGGERS",
+            "Words that count as mentioning the Snare FleaDefault:snare,flea,fleas,centipede,centipedes,HEAD,CRAB,CRABS,FACE,HUGGER,HUGGERS");
+        BunkerSpiderWords = cfg.Bind("Dictionary.Entities",
+            "BunkerSpider",
+            "bunker,spider,spiders,arachnid,arachnids,web,webs",
+            "Words that count as mentioning the Bunker SpiderDefault:bunker,spider,arachnid,web");
+        HoardingBugWords = cfg.Bind("Dictionary.Entities",
+            "HoardingBug",
+            "hoard,hoarding,bug,loot bug,LOOT",
+            "Words that count as mentioning the Hoarding BugDefault:hoard,hoarding,bug,loot bug");
+        BrackenWords = cfg.Bind("Dictionary.Entities",
+            "Bracken",
+            "bracken,flower,man,SHADOW,BLACK",
+            "Words that count as mentioning the BrackenDefault:bracken,flower,man,SHADOW,BLACK");
+        ThumperWords = cfg.Bind("Dictionary.Entities",
+            "Thumper",
+            "thumper,thumpers,halve,halves,crawler,crawlers,BEAST,TWO LEG,TWO LEGGED",
+            "Words that count as mentioning the ThumperDefault:Words that count as mentioning the BrackenDefault:bracken,flower,man,SHADOW,BLACK");
+        HygrodereWords = cfg.Bind("Dictionary.Entities",
+            "Hygrodere",
+            "hygrodere,hygroderes,slime,slimes,JELLY",
+            "Words that count as mentioning the HygrodereDefault:Words that count as mentioning the BrackenDefault:bracken,flower,man,SHADOW,BLACK");
+        GhostGirlWords = cfg.Bind("Dictionary.Entities",
+            "Ghost Girl",
+            "ghost,ghosts,girl,girls,huant,huanted,BITCH",
+            "Words that count as mentioning the Ghost GirlDefault:ghost,ghosts,girl,girls,huant,huanted,BITCH");
+        SporeLizardWords = cfg.Bind("Dictionary.Entities",
+            "Spore Lizard",
+            "spore,spores,lizard,lizards",
+            "Words that count as mentioning the Spore LizardDefault:spore,spores,lizard,lizards");
+        NutCrackerWords = cfg.Bind("Dictionary.Entities",
+            "Nutcracker",
+            "nut,nuts,cracker,crackers,shotgun,gun,soldier",
+            "Words that count as mentioning the NutcrackerDefault:nut,nuts,cracker,crackers,shotgun,gun,soldier");
+        JesterWords = cfg.Bind("Dictionary.Entities",
+            "Jester",
+            "jester,music,wind,winding",
+            "Words that count as mentioning the Jester");
+        MaskedWords = cfg.Bind("Dictionary.Entities",
+            "Masked",
+            "masked,mimic",
+            "Words that count as mentioning the Masked");
+        EyelessDogWords = cfg.Bind("Dictionary.Entities",
+            "EyelessDog",
+            "eye,eyeless,dog,dogs",
+            "Words that count as mentioning the Eyeless Dog");
+        ForestKeeperWords = cfg.Bind("Dictionary.Entities",
+            "ForestKeeper",
+            "forest,keeper,keepers,giant,giants",
+            "Words that count as mentioning the Giant");
+        EarthLeviathanWords = cfg.Bind("Dictionary.Entities",
+            "EarthLeviathan",
+            "earth,leviathan,leviathans,worm,worms",
+            "Words that count as mentioning the Earth Leviathan");
+        BaboonHawkWords = cfg.Bind("Dictionary.Entities",
+            "BaboonHawk",
+            "baboon,baboons,hawk,hawks",
+            "Words that count as mentioning the Baboon Hawk");
+        DebugSTTWords = cfg.Bind("Debug",
+            "LogSpeechToTextOutput",
+            true,
+            "Log all words and confidence");
+    }
+}
---
+++ LethalVocabulary/Plugin.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+using VoiceRecognitionAPI;
+using Random = UnityEngine.Random;
+
+namespace LethalVocabulary;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("me.loaforc.voicerecognitionapi")]
+public class Plugin : BaseUnityPlugin {
+    private const char WordSeparator = ',';
+    private static ManualLogSource _logger;
+    private static readonly Harmony Harmony = new(PluginInfo.PLUGIN_GUID);
+    private static readonly float DefaultConfidence = 0.7f;
+    private static readonly Dictionary<string, HashSet<string>> Categories = new();
+    private static readonly HashSet<string> ActiveBlacklist = new();
+
+    private static bool _checkVocabulary;
+    public new static Config Config { get; internal set; }
+
+    private void Awake() {
+        // Initialize variables
+        _logger = Logger;
+        Config = new Config(base.Config);
+        Harmony.PatchAll(typeof(Plugin));
+        LoadCategoriesFromConfig();
+
+        // Load phrases
+        Voice.RegisterPhrases(GetAllWords().ToArray());
+        Voice.RegisterCustomHandler((obj, recognizer) => {
+            if (Config.DebugSTTWords.Value) {
+                _logger.LogInfo("Heard: " + recognizer.Message + "\nConfidence: " + recognizer.Confidence);
+            }
+            if (!_checkVocabulary) return;
+
+            var player = GetCurrentPlayer();
+            if (player == null || player.health <= 0 || !StringIsIllegal(recognizer.Message) ||
+                recognizer.Confidence < DefaultConfidence) return;
+
+            CreateExplosion(player.transform.position);
+        });
+
+        _logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private static void CreateExplosion(Vector3 position) {
+        var posOffset = new Vector3(Random.Range(-0.5f, 0.5f), 0, Random.Range(-0.5f, 0.5f));
+        Landmine.SpawnExplosion(position + posOffset, true, 1f, 0f);
+    }
+
+    // Functions for managing the active blacklist
+    private static bool StringIsIllegal(string @string) {
+        foreach (var word in ActiveBlacklist)
+            if (@string.Contains(word))
+                return true;
+
+        return false;
+    }
+
+    // Returns the names of all selected categories
+    private static void LoadCategoriesFromConfig() {
+        Categories.Add("Snare Flea", GetWordsFromString(Config.SnareFleaWords.Value));
+        Categories.Add("Bunker Spider", GetWordsFromString(Config.BunkerSpiderWords.Value));
+        Categories.Add("Hoarding Bug", GetWordsFromString(Config.HoardingBugWords.Value));
+        Categories.Add("Bracken", GetWordsFromString(Config.BrackenWords.Value));
+        Categories.Add("Thumper", GetWordsFromString(Config.ThumperWords.Value));
+        Categories.Add("Hygrodere", GetWordsFromString(Config.HygrodereWords.Value));
+        Categories.Add("Ghost Girl", GetWordsFromString(Config.GhostGirlWords.Value));
+        Categories.Add("Spore Lizard", GetWordsFromString(Config.SporeLizardWords.Value));
+        Categories.Add("Nutcracker", GetWordsFromString(Config.NutCrackerWords.Value));
+        Categories.Add("Jester", GetWordsFromString(Config.JesterWords.Value));
+        Categories.Add("Masked", GetWordsFromString(Config.MaskedWords.Value));
+        Categories.Add("Eyeless Dog", GetWordsFromString(Config.EyelessDogWords.Value));
+        Categories.Add("Forest Keeper", GetWordsFromString(Config.ForestKeeperWords.Value));
+        Categories.Add("Earth Leviathan", GetWordsFromString(Config.EarthLeviathanWords.Value));
+        Categories.Add("Baboon Hawk", GetWordsFromString(Config.BaboonHawkWords.Value));
+    }
+    
+    private static List<string> CreateNewBlacklist(int numberOfCategories) {
+        List<string> selectedCategories = new();
+        var categories = Categories.Select((t, i) => Categories.ElementAt(i).Key).ToList();
+
+        // Clear the current blacklist and pick new categories
+        ActiveBlacklist.Clear();
+        for (var i = 0; i < Math.Min(numberOfCategories, Categories.Count); i++) {
+            var index = Random.RandomRangeInt(0, categories.Count);
+            selectedCategories.Add(categories[index]);
+            categories.RemoveAt(index);
+            ActiveBlacklist.UnionWith(Categories[selectedCategories.Last()]);
+        }
+
+        return selectedCategories;
+    }
+
+    // Functions for managing categories/full word list
+    public static HashSet<string> GetAllWords() {
+        HashSet<string> words = new();
+        for (var i = 0; i < Categories.Count; i++) words.UnionWith(Categories.ElementAt(i).Value);
+
+        return words;
+    }
+
+    private static HashSet<string> GetWordsFromString(string wordString) {
+        HashSet<string> words = new();
+        foreach (var word in wordString.Split(WordSeparator)) {
+            // If a word is all uppercase and extended words are not enabled, do load the word
+            if (StringIsAllUpper(word) && !Config.ExtendedWordsEnabled.Value) continue;
+            words.Add(word.ToLower());
+        }
+
+        return words;
+    }
+
+    private static bool StringIsAllUpper(string word) {
+        return word.All(c => !char.IsLetter(c) || char.IsUpper(c));
+    }
+
+    private static PlayerControllerB GetCurrentPlayer() {
+        if (RoundManager.Instance != null) return RoundManager.Instance.playersManager.localPlayerController;
+        return null;
+    }
+
+    // ==============================
+    // Patch text-based communication
+    // ==============================
+    [HarmonyPatch(typeof(HUDManager), "SubmitChat_performed")]
+    [HarmonyPrefix]
+    private static void SubmitChat_performedPatch(ref HUDManager __instance) {
+        var message = __instance.chatTextField.text.ToLower();
+        if (_checkVocabulary && StringIsIllegal(message)) {
+            CreateExplosion(__instance.localPlayer.transform.position);
+            __instance.chatTextField.text = "";
+        }
+    }
+
+    [HarmonyPatch(typeof(Terminal), "ParsePlayerSentence")]
+    [HarmonyPrefix]
+    private static void ParsePlayerTerminalPatch(ref Terminal __instance) {
+        var command = __instance.inputFieldText.text.ToLower();
+        if (_checkVocabulary && StringIsIllegal(command)) {
+            CreateExplosion(GetCurrentPlayer().transform.position);
+            if (command.StartsWith("transmit")) __instance.inputFieldText.text = "";
+        }
+    }
+
+    // =======================
+    // Patch level load states
+    // =======================
+    [HarmonyPatch(typeof(RoundManager), "LoadNewLevel")]
+    [HarmonyPrefix]
+    private static void LoadNewLevelPatch() {
+        _checkVocabulary = false;
+        _logger.LogInfo("Check Vocabulary = " + _checkVocabulary);
+    }
+
+    [HarmonyPatch(typeof(GameNetworkManager), "Disconnect")]
+    [HarmonyPostfix]
+    private static void DisconnectPatch() {
+        _checkVocabulary = false;
+        _logger.LogInfo("Check Vocabulary = " + _checkVocabulary);
+    }
+
+    [HarmonyPatch(typeof(RoundManager), "FinishGeneratingNewLevelClientRpc")]
+    [HarmonyPostfix]
+    private static void FinishGeneratingNewLevelClientRpcPatch() {
+        
+        HUDManager.Instance.DisplayTip("Active Categories:", 
+            String.Join(",", CreateNewBlacklist(Config.CategoriesPerMoon.Value)));
+        _checkVocabulary = true;
+        _logger.LogInfo("Check Vocabulary = " + _checkVocabulary);
+    }
+}

2b0d47f43d30e5bb1ee44acd0117b3b79f54be90
Sam <samtfong@hotmail.com>
2024-02-26 15:23:13 -0800

    Implement new speech recognition using an srgs document which should improve accuracy
    - Due to the new speech recognition method, it isn't possible to have the curse words setting change mid-game anymore. Changing it mid-moon will instead toggle it for the next moon.

--- LethalVocabulary/SpeechRecognizer.cs
+++ LethalVocabulary/SpeechRecognizer.cs
@@ -1,24 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Speech.Recognition;
+using System.Speech.Recognition.SrgsGrammar;
 
 namespace LethalVocabulary;
 
 public class SpeechRecognizer {
-    public static readonly HashSet<string> DefaultCommonWordsSet = new() {
-        // Words that might get used in categories later
-        "fire", "exit", "main", "land", "mine", "landmine", "turret", "door", "lock", "locked", "key", "steam", "smoke",
-        "gas", "scrap", "flash", "light", "pro", "flashlight", "stun", "grenade", "shovel", "stop", "yield", "sign",
-        "boombox", "emergency", "ladder", "tzp", "inhale", "inhalant", "moon", "land", "enemy", "pit", "gap", "quota",
-        // Other common words
-        "heck", "some", "something", "ok", "way", "hello", "hi", "hey", "lyrics", "sing", "sky", "not", "uh", "oh",
-        "hm", "for", "forever", "see", "fly", "word", "what", "look", "dead", "died", "end", "explode", "exploded",
-        "pipe", "boom", "box", "use", "drop", "miss", "really", "bro", "yea", "hard", "harder", "bright", "dark",
-        "horse", "pony", "cog", "advance", "advanced", "game", "gamer", "you", "your", "watch", "out", "smile",
-        "stupid", "doing", "job", "jump", "heavy", "weight", "weigh", "cocky", "devastated"
-    };
-
     public static readonly HashSet<string> DefaultCurseWordsSet = new() {
         "fuck", "fucker", "fucking", "motherfucker",
         "shit", "shitter", "shitting", "bullshit",
@@ -32,36 +19,37 @@ public class SpeechRecognizer {
 
     public SpeechRecognizer () {
         _recognizer.SetInputToDefaultAudioDevice();
-        _recognizer.LoadGrammar(CreateGrammar(DefaultCommonWordsSet, "default", 2));
-        _recognizer.LoadGrammar(CreateGrammar(DefaultCurseWordsSet, "curse", 0));
     }
-
+    
     public void AddSpeechRecognizedHandler (EventHandler<SpeechRecognizedEventArgs> eventHandler) {
         _recognizer.SpeechRecognized += eventHandler;
     }
 
-    public void Start () {
+    public void LoadAndStart (Grammar grammar) {
+        _recognizer.LoadGrammar(grammar);
         _recognizer.RecognizeAsync(RecognizeMode.Multiple);
     }
 
-    public void Stop () {
+    public void StopAndUnload (Grammar grammar) {
         _recognizer.RecognizeAsyncStop();
-    }
-
-    public void AddGrammar (Grammar grammar) {
-        _recognizer.LoadGrammar(grammar);
-    }
-
-    public void UnloadGrammar (Grammar grammar) {
         _recognizer.UnloadGrammar(grammar);
     }
 
-    public static Grammar CreateGrammar (IEnumerable<string> set, string name, int priority) {
-        var grammar = new Grammar(new GrammarBuilder(new Choices(set.ToArray()))) {
-            Name = name,
-            Priority = priority
-        };
+    public static SrgsDocument CreateSrgs (HashSet<string> triggerWords) {
+        SrgsRule rule = new("Rule");
+        SrgsOneOf words = new();
 
-        return grammar;
+        foreach (string word in triggerWords) {
+            words.Add(new SrgsItem(word));
+        }
+        
+        rule.Add(new SrgsItem(1, 1, SrgsRuleRef.Dictation));
+        rule.Add(new SrgsItem(0, 1, words));
+        rule.Add(new SrgsItem(0, 1, SrgsRuleRef.Dictation));
+
+        return new SrgsDocument {
+            Rules = { rule }, 
+            Root = rule
+        };
     }
 }
